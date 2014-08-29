88f58a5d75b845f2cd9932b51b3fe6c2f2555725
even <sunhongliang.cn@gmail.com>
2014-08-29 16:30:28 +0800

    要求用户输入注释信息

--- pushit.bat
+++ pushit.bat
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]
+then
+	echo "usage"
+        echo "	$0 <Ìá½»µÄ×¢ÊÍ>"
+        read commit_message
+
+else
+
+echo "first-args=$1"
 commit_message="$1"
+
+fi
+
+
 git pull
 git add . -A
 git commit -m $commit_message
